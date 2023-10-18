cad3660265d3379805c481134e62826cbd26f1a2
ishanaudichya <93155612+ishanaudichya@users.noreply.github.com>
2023-10-18 18:51:05 +0530

    gsap and nav change

--- src/Components/ImageUploader.jsx
+++ src/Components/ImageUploader.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ImageUploader.css'; // Create a separate CSS file for styling
 import { encrypt } from 'n-krypta';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
+import { gsap } from "gsap";
+
+
 const ImageUploader = () => {
+
+  const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
   const navigate = useNavigate();
   const [imageData, setImageData] = useState('');
 
   const [password, setPassword] = useState('');
   const [encryptImageData, setEncryptImageData] = useState('');
 
+  useEffect(() => {
+    tl.to(".text", { y: "0%", duration: 1, stagger: 0.25 });
+    tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 });
+    tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+    tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 
+  }, []);
 
 
   const handleImageUpload = (e) => {
@@ -45,7 +56,9 @@ const handleConvert =  ()=>{
 
   return (
     <>
-      <Navbar/><div className="container">
+    <main>
+      <Navbar/>
+      <div className="container">
   <input
     className="image-upload"
     type="file"
@@ -53,9 +66,6 @@ const handleConvert =  ()=>{
     onChange={handleImageUpload}
   />
 
-  <button className="big-button" onClick={handleConvert}>
-    Convert
-  </button>
 
   <div className="password-input">
     <input
@@ -67,6 +77,9 @@ const handleConvert =  ()=>{
       onChange={(e) => setPassword(e.target.value)}
     />
   </div>
+  <button className="big-button" onClick={handleConvert}>
+    Convert
+  </button>
 
  
 
@@ -88,9 +101,26 @@ const handleConvert =  ()=>{
   <button className="convert-button" onClick={() => navigate("/decrypt")}>
     Decrypt
   </button>
+
+
+
   
 </div>
-
+</main>
+<div class="intro">
+      <div class="intro-text">
+        <h1 class="hide">
+          <span class="text">Secure</span>
+        </h1>
+        <h1 class="hide">
+          <span class="text">and Easy to use</span>
+        </h1>
+        <h1 class="hide">
+          <span class="text">Encryption</span>
+        </h1>
+      </div>
+    </div>
+    <div class="slider"></div>
     </>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,81 +10,75 @@ function Navbar() {
   return (<>
 
     <nav className="navbar">
+
+    <div className="navbar-left"><a href="/decrypt">Decrypt</a></div>
+    <div className="navbar-center"><span className="website-name">Imgcrypt</span></div>
+    <div className='navbar-right'> <ul class="nav-links">
+            <li><a href="https://github.com/ishanaudichya">GitHub</a></li>
+            <li><a href="https://ishanaudichya.netlify.app/">Dev</a></li>
+          </ul>
+          </div>
+
+
+
+
+
+
+{/* 
       <div className="navbar-left">
-        <span className="website-name">🔒IMGCRYPT</span>
+        
       </div>
       <div className={`navbar-right ${isMobileMenuOpen ? 'mobile-menu-open' : ''}`}>
-        <a href="#">How to Use</a>
-        <a href="#">GitHub</a>
-        <a href="#">About</a>
+        
       </div>
       <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
         ☰
-      </div>
+      </div> */}
     </nav>
     <style>{`
-    .navbar {
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;1,500&family=Roboto:wght@100;300;400;500;700;900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
+  
+  .navbar {
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 10px 20px;
-  background-color: #132043;
-  color: #fff;
   height:8vh;
 }
 
 .navbar-left {
   display: flex;
   align-items: center;
-  padding-left:8vw;
-  font-family: 'Poppins', sans-serif;
-}
+  font-size: 1.2rem;
+  font-family: 'Lato', sans-serif;
+  }
 
-.website-name {
-  font-size: 24px;
-  font-weight: bold;
+.navbar-center {
+display: flex;
+align-items: center;
+font-size:3rem;
+font-family: 'Lobster', sans-serif;
 }
 
 .navbar-right {
-  font-family: 'Poppins', sans-serif;
+  font-family: 'Lato', sans-serif;
+  
+}
+.nav-links{
   display: flex;
+  list-style: none;}
+
+.nav-links li {
+  padding-left: 2rem;
+  font-size: 1.2rem;
 }
 
-.navbar-right a {
+.navbar a {
   text-decoration: none;
-  color: #fff;
-  margin-right: 20px;
 }
 
-.mobile-menu-icon {
-  display: none;
-  cursor: pointer;
-}
 
-@media (max-width: 768px) {
-  .navbar-right {
-    display: none;
-    flex-direction: column;
-    position: absolute;
-    top: 60px;
-    right: 0;
-    background-color: #333;
-    padding: 10px;
-    transition: transform 0.3s;
-    transform: translateX(100%);
-  }
-
-  .mobile-menu-open {
-    transform: translateX(0);
-    display: flex;
-  }
-
-  .mobile-menu-icon {
-    display: block;
-    font-size: 24px;
-  }
-}
 `}</style>
   </>
   );
